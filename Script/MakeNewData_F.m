f7a7739bb3a0d631a9a74537d2d879f32ec375be
zhuor <tianfc@gmail.com>
2014-11-22 21:46:37 +0800

    [mod]修改factor&dcc every的计算

---
+++ Script/MakeNewData_F.m
@@ -0,0 +1,13 @@
+function [newData,newData_F,sigma] = MakeNewData_F(data)
+%生成新的数据
+%Factor GoGarch使用
+[Var_lens,Var_cols]=size(data); %
+for i=261:Var_lens
+   for j=1: Var_cols
+       tempData=data(i-260:i,j);
+        mu=mean(tempData);
+        sigma(i-260,j)=sqrt(var(tempData));
+        newData(i-260,j)=data(i,j)-mu;
+        newData_F(i-260,j)=newData(i-260,j)/sigma(i-260,j);
+   end
+end
