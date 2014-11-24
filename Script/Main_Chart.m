fd4f0539c605b4c34c7692f99b4938e392bd9705
zhuor <tianfc@gmail.com>
2014-11-24 14:49:55 +0800

    [mod]增加参数(年数和参数)

--- Script/Main_Chart.m
+++ Script/Main_Chart.m
@@ -2,7 +2,7 @@
 clear;
 clc;
 aa=MakeChart('Alternative',1,'3');
-% aa=MakeChart('Equity',1,'f');
-% aa=MakeChart('FixIncome',1,'f');
-% aa=MakeChart('NewPF',2,'3');
-% aa=MakeChart('OldPF',2,'6');
+aa=MakeChart('Equity',1,'f');
+aa=MakeChart('FixIncome',1,'f');
+aa=MakeChart('NewPF',2,'3');
+aa=MakeChart('OldPF',2,'6');
